bb7ae9b750211cd5e1125dc21b321fcb3deca6be
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-03-31 21:37:40 +0200

    CP GIT

--- build/derniers-articles/index.asset.php
+++ build/derniers-articles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '80b5099118c3ce2f0f1d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '10ddc84e295a72bfa3a6');
