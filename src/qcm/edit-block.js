aa2409d3658d27c2192bbfe42cd92925e456b1a2
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-04-03 23:37:11 +0200

    CP GIT

--- src/qcm/edit-block.js
+++ src/qcm/edit-block.js
@@ -2,8 +2,6 @@ export default function Block(props) {
 	const { blockProps } = props;
 
 	return (
-		<div {...blockProps} >
-			<p>Emplacement réservé au QCM.</p>
-		</div>
+		<div {...blockProps} ></div>
 	);
 }
